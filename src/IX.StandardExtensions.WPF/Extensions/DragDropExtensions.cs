635a8d9a5b8daa9af6604d6df0d09ca04876f9ee
Adrian Mos <adimosh@outlook.com>
2021-12-30 13:54:38 +0200

    A few missed Drag&Drop extensions.

--- src/IX.StandardExtensions.WPF/Extensions/DragDropExtensions.cs
+++ src/IX.StandardExtensions.WPF/Extensions/DragDropExtensions.cs
@@ -32,5 +32,46 @@ namespace IX.StandardExtensions.WPF.Extensions
 
             return convertedObject;
         }
+
+        /// <summary>
+        /// Gets an object of a given type from the drag&amp;drop data object.
+        /// </summary>
+        /// <typeparam name="T">The type of object to get.</typeparam>
+        /// <param name="dataObject">The data object to get from.</param>
+        /// <param name="format">The string format representation.</param>
+        /// <returns>An object of the desired type, if one was contained in the data object.</returns>
+        /// <exception cref="InvalidCastException">The raw object cannot be converted to the desired type.</exception>
+        public static T GetData<T>(this IDataObject dataObject, string format)
+        {
+            object rawObject = dataObject.GetData(format);
+
+            if (rawObject is not T convertedObject)
+            {
+                throw new InvalidCastException();
+            }
+
+            return convertedObject;
+        }
+
+        /// <summary>
+        /// Gets an object of a given type from the drag&amp;drop data object.
+        /// </summary>
+        /// <typeparam name="T">The type of object to get.</typeparam>
+        /// <param name="dataObject">The data object to get from.</param>
+        /// <param name="format">The string format representation.</param>
+        /// <param name="autoConvert">Automatically convert to the desired format.</param>
+        /// <returns>An object of the desired type, if one was contained in the data object.</returns>
+        /// <exception cref="InvalidCastException">The raw object cannot be converted to the desired type.</exception>
+        public static T GetData<T>(this IDataObject dataObject, string format, bool autoConvert)
+        {
+            object rawObject = dataObject.GetData(format, autoConvert);
+
+            if (rawObject is not T convertedObject)
+            {
+                throw new InvalidCastException();
+            }
+
+            return convertedObject;
+        }
     }
 }
